303d2c61c9f4c1aa548f2532e4cd83a87ea33476
Shahdabukishik <s12325651@stu.najah.edu>
2025-04-09 00:25:33 +0300

    done

--- frontend/src/app/welcome-page/welcome-page.component.ts
+++ frontend/src/app/welcome-page/welcome-page.component.ts
@@ -1,6 +1,5 @@
 import {Component, HostListener} from '@angular/core';
 import {HeaderComponent} from './header/header.component';
-import {NgIf} from '@angular/common';
 import {StatisticsComponent} from './statistics/statistics.component';
 
 
@@ -8,18 +7,12 @@ import {StatisticsComponent} from './statistics/statistics.component';
   selector: 'app-welcome-page',
   imports: [
     HeaderComponent,
-    NgIf,
     StatisticsComponent
   ],
   templateUrl: './welcome-page.component.html',
   styleUrl: './welcome-page.component.css'
 })
 export class WelcomePageComponent {
-  scrolled=false;
 
-  @HostListener('window:scroll',[])
-  onScroll() {
-    const ScrollTop = window.scrollY || document.documentElement.scrollTop;
-    this.scrolled = ScrollTop > 50;
-  }
+
 }
