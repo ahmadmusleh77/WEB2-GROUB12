d30b9636b7a4e2e3e167bfb788765662f517fa5b
Shahdabukishik <s12325651@stu.najah.edu>
2025-04-10 23:21:29 +0300

    who we are component it's done

--- frontend/src/app/welcome-page/welcome-page.component.ts
+++ frontend/src/app/welcome-page/welcome-page.component.ts
@@ -1,13 +1,15 @@
 import {Component, HostListener} from '@angular/core';
 import {HeaderComponent} from './header/header.component';
 import {StatisticsComponent} from './statistics/statistics.component';
+import {WhoWeAreComponent} from './who-we-are/who-we-are.component';
 
 
 @Component({
   selector: 'app-welcome-page',
   imports: [
     HeaderComponent,
-    StatisticsComponent
+    StatisticsComponent,
+    WhoWeAreComponent
   ],
   templateUrl: './welcome-page.component.html',
   styleUrl: './welcome-page.component.css'
