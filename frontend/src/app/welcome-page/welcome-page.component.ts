b695b81332403694d8c5dcb323e9f3b3ae0a1301
Shahdabukishik <s12325651@stu.najah.edu>
2025-04-11 01:22:43 +0300

    footer for welcome page is done

--- frontend/src/app/welcome-page/welcome-page.component.ts
+++ frontend/src/app/welcome-page/welcome-page.component.ts
@@ -1,15 +1,19 @@
-import {Component, HostListener} from '@angular/core';
+import {Component} from '@angular/core';
 import {HeaderComponent} from './header/header.component';
 import {StatisticsComponent} from './statistics/statistics.component';
 import {WhoWeAreComponent} from './who-we-are/who-we-are.component';
+import {FooterWelcomeComponent} from './footer-welcome/footer-welcome.component';
+
 
 
 @Component({
   selector: 'app-welcome-page',
   imports: [
     HeaderComponent,
+    WhoWeAreComponent,
     StatisticsComponent,
-    WhoWeAreComponent
+    FooterWelcomeComponent,
+
   ],
   templateUrl: './welcome-page.component.html',
   styleUrl: './welcome-page.component.css'
