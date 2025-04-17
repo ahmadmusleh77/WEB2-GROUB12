76fcade18c7fe992bad7cb0e3d52a013f297bd74
ahmad musleh <s12217787@stu.najah.edu>
2025-04-17 22:48:15 +0300

    routing taske

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -22,33 +22,27 @@ import { ManageBidsComponent } from './admin/manage-bids/manage-bids.component';
 import { AccountProfileComponent } from './admin/profile-admin/account-admin/account-admin.component';
 import { SettingsComponent } from './admin/profile-admin/settings-admin/settings-admin.component';
 import {OfferJobownerComponent} from './job-owner/offer-jobowner/offer-jobowner.component';
-import {EditPostComponent} from './job-owner/edit-post/edit-post.component';
-import {CreatePostComponent} from './job-owner/create-post/create-post.component';
-import {ManageBidsJobownerComponent} from './job-owner/manage-bids-jobowner/manage-bids-jobowner.component';
 import {JobOwnerComponent} from './job-owner/job-owner.component';
-import {NavbarJobownerComponent} from './job-owner/navbar-jobowner/navbar-jobowner.component';
+import {CreatePostComponent} from './job-owner/create-post/create-post.component';
+import {EditPostComponent} from './job-owner/edit-post/edit-post.component';
+import {HomeJobownersComponent} from './job-owner/home-jobowners/home-jobowners.component';
+import {ManageBidsJobownerComponent} from './job-owner/manage-bids-jobowner/manage-bids-jobowner.component';
+import {ChatJobOwnerComponent} from './job-owner/chat-job-owner/chat-job-owner.component';
+
 
 export const routes: Routes = [
-
-  {path: 'offer-jobowner',component: OfferJobownerComponent},
-  {path: 'edit-post',component: EditPostComponent},
-  {path: 'create-post',component: CreatePostComponent},
-  {path: 'manage-bids-jobowner',component: ManageBidsJobownerComponent},
-  {path: 'job-owner',component: JobOwnerComponent},
-  {path: 'navbar-jobowner',component: NavbarJobownerComponent},
-
 
   {
     path: '',
     component: AuthComponent,
     children: [
-      { path: '', redirectTo: 'welcomepage', pathMatch: 'full' },
-      { path: 'welcomepage', component: WelcomePageComponent },
+      { path: '', redirectTo: 'welcome-page', pathMatch: 'full' },
+      { path: 'welcome-page', component: WelcomePageComponent },
       { path: 'login', component: LoginComponent },
-      { path: 'createaccount', component: CreateAccountComponent },
-      { path: 'forgotpassword', component: ForgotPasswordComponent },
+      { path: 'create-account', component: CreateAccountComponent },
+      { path: 'forget-password', component: ForgotPasswordComponent },
       { path: 'verification', component: OtpVerificationComponent },
-      { path: 'newpassword', component: NewPasswordComponent }
+      { path: 'new-password', component: NewPasswordComponent }
     ],
 
   },
@@ -56,9 +50,9 @@ export const routes: Routes = [
     path: 'artisans-dashboard',
     component: ArtisansComponent,
     children: [
-      { path: '', redirectTo: 'home', pathMatch: 'full' },
-      { path: 'home', component: HomeComponent },
-      { path: 'chat', component: ChatComponent },
+      { path: '', redirectTo: 'home-artisan', pathMatch: 'full' },
+      { path: 'home-artisan', component: HomeComponent },
+      { path: 'chat-artisan', component: ChatComponent },
       { path: 'all-posts', component: AllPostComponent },
       { path: 'offers', component: OfferArtisanComponent },
       { path: 'profile', component: ArtisanAccountProfileComponent },
@@ -78,6 +72,23 @@ export const routes: Routes = [
       { path: 'profile', component: AccountProfileComponent },
       { path: 'settings', component: SettingsComponent }
     ]
-  }
+  },
+  {
+    path: 'job-owner-dashboard',
+    component :JobOwnerComponent,
+    children :[
+      {path: '', redirectTo: 'home-job-owner', pathMatch: 'full'},
+      { path: 'home-job-owner', component: HomeJobownersComponent },
+      {path: 'offer-job-owner',component: OfferJobownerComponent},
+      {path: 'creat-posts',component: CreatePostComponent},
+      {path: 'edit-post',component: EditPostComponent},
+      {path:'manage-bids-job-owner',component: ManageBidsJobownerComponent},
+      {path:'edit-post',component: EditPostComponent},
+      {path:'profile-job-owner',component:AccountProfileComponent},
+      {path:'settings-job-owner',component:SettingsComponent},
+      { path: 'chat-job-owner', component:ChatJobOwnerComponent},
+
+    ]
+  },
 
 ];
