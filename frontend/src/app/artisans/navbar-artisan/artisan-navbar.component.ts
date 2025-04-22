689f400912522764ce2ff3ab1ea2327b941054f1
Shahdabukishik <s12325651@stu.najah.edu>
2025-04-22 16:06:58 +0300

    done

--- frontend/src/app/artisans/navbar-artisan/artisan-navbar.component.ts
+++ frontend/src/app/artisans/navbar-artisan/artisan-navbar.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import {RouterLink, RouterLinkActive} from '@angular/router';
 
+
 @Component({
   selector: 'app-navbar-artisan',
-
   imports: [
     RouterLink,
     RouterLinkActive,
